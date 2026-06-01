faaedc813a855277efe58d1ec71e8213d1488041
nourhassan94 <nourhassan94@hotmail.com>
2026-06-01 23:17:13 +0200

    Adderar github action CI pipeline

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.12-slim
+
+WORKDIR /app
+
+COPY . .
+
+RUN pip install -r requirements.txt
+
+CMD ["python", "calculator.py"]
+
--- calculator.py
+++ calculator.py
@@ -4,13 +4,16 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
-def multitply(a, b):
+def multiply(a, b):
     return a * b
 
 def divide(a, b):
     return a / b
     
-if __name__ == "__main__"
+if __name__ == "__main__":
     print(add(5, 3))
+    print(subtract(5, 2))
+    print(multiply (5,5))
+    print(divide(25,4))
 
 
